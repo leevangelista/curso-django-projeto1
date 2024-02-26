0f57692f5f1e028e93159e90b2ac75866efa1886
Leonardo <le-evangelista@hotmail.com>
2024-02-26 19:51:06 -0300

    tratamento de erro da pagina recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import get_object_or_404, render, get_list_or_404
 from utils.recipes.factory import make_recipe
 from django.http import Http404
 from .models import Recipe
@@ -27,10 +27,12 @@ def category(request, category_id):
     })
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id,
-        is_published=True,
-    ).order_by('-id').first()
+    # recipe = Recipe.objects.filter(
+    #     pk=id,
+    #     is_published=True,
+    # ).order_by('-id').first()
+
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,

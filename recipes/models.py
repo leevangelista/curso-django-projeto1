36b14b5ee2cfe97135f97b93149d396419de835d
Leonardo <le-evangelista@hotmail.com>
2024-03-12 00:20:36 -0300

    testes em models

--- recipes/models.py
+++ recipes/models.py
@@ -19,7 +19,7 @@ class Recipe(models.Model):
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d', blank=True, default='')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True, blank=True, default=None
